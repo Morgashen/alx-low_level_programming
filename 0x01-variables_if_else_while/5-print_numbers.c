627ade69186de360eb5590096a0e5b3d2e32fe5b
morgashen <mashudumorgan@gmail.com>
2023-06-17 10:21:44 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always return 0 (success)
+ */
+int main(void)
+{
+int 10 = 0;
+while (10 < 10)
+{
+putchar('0' + 10);
+10++;
+}
+putchar('\n');
+return (0);
+}
