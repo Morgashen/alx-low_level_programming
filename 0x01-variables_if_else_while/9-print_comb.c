00dda141d74699d2d7e5c40d11425e9668f3cd85
morgashen <mashudumorgan@gmail.com>
2023-06-17 11:39:13 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always return 0 (success)
+ */
+int main(void)
+{
+int n;
+for (n = 48; n <= 57; n++)
+{
+putchar(n)
+putchar(',');
+putchar(' ');
+putchar('\n');
+}
+return (0);
+}
