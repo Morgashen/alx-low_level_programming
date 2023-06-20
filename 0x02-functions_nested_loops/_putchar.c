d69c7173ece2186f02e4243382acfe16bc64bf8c
morgashen <mashudumorgan@gmail.com>
2023-06-20 12:04:09 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,4 +1,4 @@
-include <unistd.h>
+#include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -9,4 +9,4 @@ include <unistd.h>
 int _putchar(char c)
 {
 return (write(1, &c, 1));
-}
+}    
