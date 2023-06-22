507a8eef9a5f7c53f236320062a4065365dbb119
morgashen <mashudumorgan@gmail.com>
2023-06-22 09:42:11 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -1,40 +1,27 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
- * print_remaining_days - Prints the remaining days of a given year
- * @month: The month (1-12)
- * @day: The day (1-31)
- * @year: The year
- *
- * Description: Calculates the number of remaining days in a year
- *              considering if it's a leap year or not.
- */
-void print_remaining_days(int month, int day, int year)
-{
-    int days_in_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    int total_days = 0;
-    int i;
-
-    for (i = 0; i < month - 1; i++)
-        total_days += days_in_month[i];
-
-    total_days += day;
-
-    if (month > 2 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0))
-        total_days++;
-
-    printf("Day of the year: %d\n", total_days);
-}
+* main - takes a date and prints how many days are left in the year, taking
+* leap years into account
+* Return: 0
+*/
 
 int main(void)
 {
-    int month, day, year;
+    int month;
+    int day;
+    int year;
 
-    printf("Enter a date (mm/dd/yyyy): ");
-    scanf("%d/%d/%d", &month, &day, &year);
+    month = 02;
+    day = 29;
+    year = 2000;
+
+    printf("Date: %02d/%02d/%04d\n", month, day, year);
+
+    day = convert_day(month, day);
 
     print_remaining_days(month, day, year);
 
-    return 0;
+    return (0);
 }
-
